a2ac3c83873b1a55004603413f681e6f6037c83b
PalumboN <nahuel.palumbo@gmail.com>
2016-04-02 20:50:01 -0300

    Testing EmptyParser

--- src/test/scala/org/uqbar/thin/more/ParsersTests.scala
+++ src/test/scala/org/uqbar/thin/more/ParsersTests.scala
@@ -1,3 +1,37 @@
 package org.uqbar.thin.more
 
-// TODO: test parser combinators
+import org.scalatest.Finders
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers
+import org.scalatest.matchers.MatchResult
+import org.scalatest.matchers.Matcher
+import org.uqbar.thin.more.views.source.Parsers
+
+class JavalessParserTest extends FreeSpec with Matchers with Parsers {
+
+  "Parser" - {
+
+    "EmptyParser" - {
+      implicit val parser = EmptyParser
+
+      "should succeed for empty string" in {
+        "" should beParsedTo(null)
+      }
+      
+      "should fail for non empty string" in {
+        "somethign" shouldNot beParsedTo(null)
+      }
+    }
+  }
+}
+
+case class beParsedTo[T](expected: T)(implicit parser: Parsers#CodeParser[T]) extends Matcher[String] {
+  def apply(target: String) = {
+    val result = parser.apply(target)
+
+    MatchResult(
+      result.map { parsed => parsed == expected }.getOrElse(false),
+      result.map { parsed => s"Parsed $parsed did not equal $expected" }.getOrElse(s"Parse failed! $result"),
+      result.map { parsed => s"Parsed $parsed was equal to $expected" }.getOrElse(s"Parse didn't fail! $result"))
+  }
+}  
