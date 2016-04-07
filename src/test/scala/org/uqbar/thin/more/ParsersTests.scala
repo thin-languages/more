8593009a2f649a834679bf267e3ea6ed9a62bf63
PalumboN <nahuel.palumbo@gmail.com>
2016-04-07 02:52:14 -0300

    Testing TerminalParser

--- src/test/scala/org/uqbar/thin/more/ParsersTests.scala
+++ src/test/scala/org/uqbar/thin/more/ParsersTests.scala
@@ -6,6 +6,8 @@ import org.scalatest.Matchers
 import org.scalatest.matchers.MatchResult
 import org.scalatest.matchers.Matcher
 import org.uqbar.thin.more.views.source.Parsers
+import org.uqbar.thin.more.views.source.GrammarPreferences
+import org.uqbar.thin.more.views.source.EncodingPreferences
 
 class JavalessParserTest extends FreeSpec with Matchers with Parsers {
 
@@ -17,21 +19,43 @@ class JavalessParserTest extends FreeSpec with Matchers with Parsers {
       "should success for empty string" in {
         "" should beParsedTo(null)
       }
-      
+
       "should fail for non empty string" in {
         "somethign" shouldNot beParsedTo(null)
       }
     }
-    
+
     "ValueParser" - {
       implicit val parser = new ValueParser("[a-z]+".r)
 
       "should success when string matchs the regex" in {
         "var" should beParsedTo("var")
       }
-      
+
       "should fail when string doesn't match the regex" in {
-        "Var" shouldNot beParsedTo("var2")
+        "Var" shouldNot beParsedTo("")
+      }
+    }
+
+    "TerminalParser" - {
+      val terminals = Map[Symbol, String](
+        'Foo -> "Foo",
+        'X -> "X:",
+        ': -> ":")
+
+      implicit val options = GrammarPreferences(terminals, new EncodingPreferences)
+      implicit val parser = new TerminalParser('Foo)
+
+      "should success when string matchs with terminal" in {
+        "Foo" should beParsedTo("Foo")
+      }
+
+      "should fail when string doesn't match with terminal" in {
+        "foo" shouldNot beParsedTo("")
+      }
+
+      "should fail when string matchs other terminal" in {
+        "X" shouldNot beParsedTo("")
       }
     }
   }
